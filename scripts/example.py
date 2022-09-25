0c64d7fa3f4437ae82b39eab0dd3ffffa7638413
william831015 <leepowan831015@email.com>
2022-09-25 09:00:43 +0200

    20220923

--- scripts/example.py
+++ scripts/example.py
@@ -1,3 +1,37 @@
-import models
-import species
-import functions
+import scripts.models
+import scripts.species
+import scripts.functions
+
+#######Define species #######
+
+#T7 RNAP
+T7_DNA = DNA()
+T7_RNA = RNA()
+T7 = Protein()
+
+#Repressor
+Repressor_DNA = DNA()
+Repressor_RNA = RNA()
+Repressor = Protein()
+
+#GFP
+GFP_DNA = DNA()
+GFP_RNA = RNA()
+GFP = Protein()
+
+#Resource
+R      = Resource()
+
+
+
+########define parameters #######
+
+
+
+
+########define models #######
+
+
+
+
+########solve models #######
