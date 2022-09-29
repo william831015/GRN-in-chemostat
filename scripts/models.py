3bc31ecc32e04527f0c4324db1ed68d3fa9b3c90
william831015 <leepowan831015@gmail.com>
2022-09-29 15:44:22 +0200

    com

--- scripts/models.py
+++ scripts/models.py
@@ -1,8 +1,11 @@
+#ODEs of different models
+
+#The simplest model of resource dependent model
 def resource_dependent_model(molecules,t,alpha,beta,K,KTL,KTX,lamb1,lamb2):
     R,dT,dG,mT,mG,pT,pG,TX,TL = molecules
     return np.array(
         [
-            -alpha*TX*dT/(dT+dG+KTX)*pT*R/(R+K)-alpha*TX*dG/(dT+dG+KTX)*pT*R/(R+K)-beta*TL*mT/(mT+mG+KTL)*R/(R+K)-beta*TL*mG/(mT+mG+KTL)*R/(R+K),
+            -alpha*TX*dT*pT*R/(R+K)-alpha*TX*dG*pT*R/(R+K)-beta*TL*mT*R/(R+K)-beta*TL*mG*R/(R+K),
             0,
             0,
             alpha*TX*pT*R/(R+K),
@@ -14,7 +17,7 @@ def resource_dependent_model(molecules,t,alpha,beta,K,KTL,KTX,lamb1,lamb2):
         ]
     )
     
-
+#resource dependent model that include saturation effect 
 def resource_dependent_saturation_model(molecules,t,alpha,beta,K,KTL,KTX,lamb1,lamb2):
     R,dT,dG,mT,mG,pT,pG,TX,TL = molecules
     return np.array(
