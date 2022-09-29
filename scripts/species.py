aaa35bd1c95853eba3bfe238572332fd38b3d4ba
william831015 <leepowan831015@gmail.com>
2022-09-29 15:45:23 +0200

    cs

--- scripts/species.py
+++ scripts/species.py
@@ -1,8 +1,10 @@
 #class of different species
 # ic : initial concentration
 # lc : loading concentration
-# pr : production rate 
+# txr : transcription rate 
+# tlr : translation rate 
 # dr : degradation rate
+
 class DNA:
     ic = 0
     lc = 0
@@ -10,16 +12,16 @@ class DNA:
 
 
 class RNA:
+    txr = 1
     ic = 0
     lc = 0
-    pr = 0
     dr = 0
 
 
 class Protein: 
+    tlr = 1
     ic = 0
     lc = 0
-    pr = 0
     dr = 0
 
 class Resource:
