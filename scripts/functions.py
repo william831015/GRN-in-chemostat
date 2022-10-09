de43123c468fea2c2589cd0266e9505d8677d8c6
william831015 <leepowan831015@email.com>
2022-10-09 22:14:35 +0200

    repressor

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,16 +1,15 @@
-def dilution(molecule, dilution_ratio):
+def dilution(molecule, DR=0.2):
 
-    molecule_diluted = molecule*(1-dilution_ratio)
+    molecule_diluted = molecule*(1-DR)
 
     return molecule_diluted
 
-
+def dilute_out_species(molecules):
+    for i in (molecules):
+        print(i)
 
 def replenish(molecule_replenished, dilution_ratio, replenish_conc) : 
 
     molecule_replenished = replenish_conc * dilution_ratio
 
-    return molecule_replenished;
-
-
-
+    return molecule_replenished
