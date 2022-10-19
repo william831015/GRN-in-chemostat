2bdccdca7d5d18ae128385d8ce55cbf7956a9b62
william831015 <leepowan831015@email.com>
2022-10-19 08:21:32 +0200

    update

--- scripts/models.py
+++ scripts/models.py
@@ -45,7 +45,7 @@ def Repressor_model(molecules,t,alpha,beta,K,Kr,KTL,KTX,lamb1,lamb2):
     return np.array(
         [
             0,
-            alpha*TX*T7_DNA*T7*R/(R+K)*(Kr/(Repressor+Kr)),
+            alpha*TX*T7_DNA*T7*R/(R+K)*(Kr**0.2/(Repressor+Kr**0.2)),
             beta*TL*T7_RNA*(R/(R+K)),
             0,
             alpha*TX*GFP_DNA*T7*R/(R+K),
